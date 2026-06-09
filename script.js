515a6f838dd260698a8c203a8e33340a653e8018
ZeagaBG <muratnasuf69@gmail.com>
2026-06-09 22:36:16 +0300

    ?

---
+++ script.js
@@ -0,0 +1,22 @@
+const stars = document.getElementById("stars");
+
+for(let i = 0; i < 200; i++){
+
+    const star = document.createElement("div");
+
+    star.style.position = "absolute";
+    star.style.width = "2px";
+    star.style.height = "2px";
+    star.style.background = "white";
+
+    star.style.left =
+        Math.random() * 100 + "%";
+
+    star.style.top =
+        Math.random() * 100 + "%";
+
+    star.style.opacity =
+        Math.random();
+
+    stars.appendChild(star);
+}
